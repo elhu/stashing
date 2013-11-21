f43b3dcacd10a6d84f61572d3710ae30b5ae0bb1
Vincent Boisard <vincent.boisard@dimelo.com>
2013-11-21 17:29:01 +0100

    fix: removed missed references to Logtastic

--- spec/stashing/stashing_spec.rb
+++ spec/stashing/stashing_spec.rb
@@ -3,11 +3,11 @@ require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 describe 'Stashing' do
   describe '#store' do
     it "returns a new Hash for each key" do
-      expect(Stashing.store['a'].object_id).to_not be(Stashing.store['b'].object_id)
+      expect(Stashing.stash['a'].object_id).to_not be(Stashing.stash['b'].object_id)
     end
 
     it "returns the same stash if called several time with the same key" do
-      expect(Stashing.store['a'].object_id).to be(Stashing.store['a'].object_id)
+      expect(Stashing.stash['a'].object_id).to be(Stashing.stash['a'].object_id)
     end
   end
 
